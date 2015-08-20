417f4dc8e1767b35557b0fcd775422c1dfef77a0
Bluespeedgamer <github@bluespeed.us>
2015-08-19 21:26:33 -0400

    Added a few phrases, fixed duplicate issue

--- scripts/init.js
+++ scripts/init.js
@@ -3,11 +3,10 @@ var app = angular.module('BINGO',[]);
 app.factory('BingoPhrases',['$log',function($log){
 	var list = [];
 					var array = AllPhrases.list;
-					var length = array.length;
-					//$log.debug(data);
-						for(i =0; i < 25;i++){
-							var index = Math.floor(Math.random() * (length));
+						for(i =0; i < 24;i++){
+							var index = Math.floor(Math.random() * (array.length));
 							list[i] =  {"Found":0,"Index":i,"Phrase":array[index]};
+							array.splice(index,1);
 						}
 	
 	
